ed7b714c3fae628df7b946fc1797dc15820feef7
Montse Viyuela <montsemsanz@gmail.com>
2021-09-28 17:36:52 +0200

    Primer commit

---
+++ TestEstudiante.java
@@ -0,0 +1,22 @@
+
+/**
+ *  Para probar Git Github desde BlueJ
+ *  
+ */
+public class TestEstudiante
+{
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+        System.out.println("Creamos varios objetos Estudiante");
+        Estudiante e1 = new Estudiante("Alberto", 6);
+        e1.printEstudiante();
+        Estudiante e2 = new Estudiante("Ana", 8);
+        e2.printEstudiante();
+		Estudiante e3 = new Estudiante("Elisa", 26);
+		e3.printEstudiante();
+
+    }
+}
